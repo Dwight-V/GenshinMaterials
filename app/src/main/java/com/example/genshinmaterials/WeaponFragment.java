c9aab3e5f45364dd2058d7b666f8f4e5fdfbaf4f
Dwight-V <dvight@duck.com>
2024-09-02 15:30:57 -0400

    API calls work for WeaponFragment

--- app/src/main/java/com/example/genshinmaterials/WeaponFragment.java
+++ app/src/main/java/com/example/genshinmaterials/WeaponFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,11 +24,22 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.tabs.TabLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class WeaponFragment extends CounterFragment {
 
@@ -59,6 +71,13 @@ public class WeaponFragment extends CounterFragment {
             {0, 0, 12, 10, 6}};
 
 
+    private String requestUrl2 = "https://genshin.jmp.blue/materials/common-ascension";
+
+    private String requestUrl1 = "https://genshin.jmp.blue/materials/common-ascension";
+
+    private String requestUrl0 = "https://genshin.jmp.blue/materials/weapon-ascension";
+
+
     // Passes the final String array which names all EditTexts save data.
     WeaponFragment() {
         super(EDITTEXT_VALUES_0, EDITTEXT_VALUES_1, EDITTEXT_VALUES_2, tabNamesArr, reqMats5Star, SUBTAB_POSITION, ITEM_RARITY);
@@ -70,12 +89,6 @@ public class WeaponFragment extends CounterFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        View view = inflater.inflate(R.layout.fragment_counter, container, false);
         View view = super.onCreateView(inflater, container, savedInstanceState);
-
-        txtTypeTitle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
 
         return view;
     }
@@ -106,5 +119,259 @@ public class WeaponFragment extends CounterFragment {
         }
 
         super.checkRequirements();
+    }
+
+    @Override
+    public void updateCounterUi() {
+        super.updateCounterUi();
+
+        switch (tabMaterials.getSelectedTabPosition()) {
+            case 2:
+                updateCounterIconsTab2();
+                break;
+            case 1:
+                updateCounterIconsTab1();
+                break;
+            default:
+                updateCounterIconsTab0();
+                break;
+        }
+    }
+
+
+    public void updateCounterIconsTab2() {
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getContext());
+        JsonObjectRequest mJsonRequest = new JsonObjectRequest(requestUrl2, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // this list of ArrayLists will hold the 3 rarity types of enemy drops, blue, green, and grey.
+                ArrayList<String>[] possibleIcons = new ArrayList[3];
+                JSONArray categoriesArr;
+
+                // Initializes possibleIcons.
+                for (int k = 0; k < possibleIcons.length; k++) {
+                    possibleIcons[k] = new ArrayList<String>();
+                }
+
+//                txtStatic.setText(response.names().toString());
+                // Turns the JSONObject response into a JSONArray called categoriesArr.
+                try {
+                    categoriesArr = response.toJSONArray(response.names());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Go through all the categories in response.
+                for (int i = 0; i < categoriesArr.length(); i++) {
+                    try {
+                        JSONObject curCategory = categoriesArr.getJSONObject(i);
+
+                        // First check if this material type is for upgrading characters (there were some weapon only materials when I looked through the response).
+                        if (curCategory.getString("weapons") != null) {
+                            JSONArray itemArr = curCategory.getJSONArray("items");
+                            // This means the item is a "miniboss" item, as it doesn't have a grey rarity version.
+                            if (itemArr.getJSONObject(0).getInt("rarity") != 1) {
+                                continue;
+                            }
+                            // Each item has an attribute called "rarity", and I use that to sort them into the correct ArrayList.
+                            for (int j = 0; j < itemArr.length(); j++) {
+                                JSONObject curItem = itemArr.getJSONObject(j);
+                                // I reversed the order (rarity 3 = blue, but put into possibleIcons[0]) because
+                                // the for loop that puts the images into the ImageViews below,
+                                // starts with the highest rarity at the lowest index, mirroring allImgViewIcons[].
+                                switch (curItem.getInt("rarity")) {
+                                    case 3:
+                                        possibleIcons[0].add(curItem.getString("id"));
+                                        break;
+                                    case 2:
+                                        possibleIcons[1].add(curItem.getString("id"));
+                                        break;
+                                    case 1:
+                                        possibleIcons[2].add(curItem.getString("id"));
+                                        break;
+                                }
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        Log.i("API", "categoriesArr loop failed: " + e.toString());
+                    }
+                }
+//                txtStatic.setText(possibleIcons[0].toString() + "\n" + possibleIcons[1].toString() + "\n" + possibleIcons[2].toString());
+
+//                Toast.makeText(getContext(), possibleIcons[0].size() + " " + possibleIcons[1].size() + " " + possibleIcons[2].size(), Toast.LENGTH_SHORT).show();
+                // Skips imgViewYellow & imgViewPurple.
+                for (int i = 0; (i + 2) < allImgViewIcons.length; i++) {
+                    if ((i + 2) < allImgViewIcons.length) {
+                        // Uses superclass' method to display the images.
+                        updateCounterIcon(allImgViewIcons[i + 2], requestUrl2 + "/" + possibleIcons[i].get((int) (Math.random() * possibleIcons[i].size())));
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "API request for image failed.", Toast.LENGTH_SHORT).show();
+                Log.i("API", "Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mJsonRequest);
+    }
+    public void updateCounterIconsTab1() {
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getContext());
+        JsonObjectRequest mJsonRequest = new JsonObjectRequest(requestUrl1, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // this list of ArrayLists will hold the 3 rarity types of enemy drops, blue, green, and grey.
+                ArrayList<String>[] possibleIcons = new ArrayList[3];
+                JSONArray categoriesArr;
+
+                // Initializes possibleIcons.
+                for (int k = 0; k < possibleIcons.length; k++) {
+                    possibleIcons[k] = new ArrayList<String>();
+                }
+
+//                txtStatic.setText(response.names().toString());
+                // Turns the JSONObject response into a JSONArray called categoriesArr.
+                try {
+                    categoriesArr = response.toJSONArray(response.names());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Go through all the categories in response.
+                for (int i = 0; i < categoriesArr.length(); i++) {
+                    try {
+                        JSONObject curCategory = categoriesArr.getJSONObject(i);
+
+                        // First check if this material type is for upgrading characters (there were some weapon only materials when I looked through the response).
+                        if (curCategory.getString("weapons") != null) {
+                            JSONArray itemArr = curCategory.getJSONArray("items");
+
+                            // This means the item isn't a "miniboss" item, as it does have a grey rarity version.
+                            if (itemArr.getJSONObject(0).getInt("rarity") == 1) {
+                                continue;
+                            }
+
+                            // Each item has an attribute called "rarity", and I use that to sort them into the correct ArrayList.
+                            for (int j = 0; j < itemArr.length(); j++) {
+                                JSONObject curItem = itemArr.getJSONObject(j);
+                                // I reversed the order (rarity 3 = blue, but put into possibleIcons[0]) because
+                                // the for loop that puts the images into the ImageViews below,
+                                // starts with the highest rarity at the lowest index, mirroring allImgViewIcons[].
+                                switch (curItem.getInt("rarity")) {
+                                    case 4:
+                                        possibleIcons[0].add(curItem.getString("id"));
+                                        break;
+                                    case 3:
+                                        possibleIcons[1].add(curItem.getString("id"));
+                                        break;
+                                    case 2:
+                                        possibleIcons[2].add(curItem.getString("id"));
+                                        break;
+                                }
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        Log.i("API", "categoriesArr loop failed: " + e.toString());
+                    }
+                }
+//                txtStatic.setText(possibleIcons[0].toString() + "\n" + possibleIcons[1].toString() + "\n" + possibleIcons[2].toString());
+
+                // Skips imgViewYellow & imgViewPurple.
+                for (int i = 0; (i + 1) < allImgViewIcons.length - 1; i++) {
+                    if ((i + 1) < allImgViewIcons.length) {
+                        // Uses superclass' method to display the images.
+                        updateCounterIcon(allImgViewIcons[i + 1], requestUrl1 + "/" + possibleIcons[i].get((int) (Math.random() * possibleIcons[i].size())));
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "API request for image failed.", Toast.LENGTH_SHORT).show();
+                Log.i("API", "Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mJsonRequest);
+    }
+
+    public void updateCounterIconsTab0() {
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getContext());
+        JsonObjectRequest mJsonRequest = new JsonObjectRequest(requestUrl0, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // this list of ArrayLists will hold the 3 rarity types of enemy drops, blue, green, and grey.
+                ArrayList<String>[] possibleIcons = new ArrayList[4];
+                JSONArray categoriesArr;
+
+                // Initializes possibleIcons.
+                for (int k = 0; k < possibleIcons.length; k++) {
+                    possibleIcons[k] = new ArrayList<String>();
+                }
+
+//                txtStatic.setText(response.names().toString());
+                // Turns the JSONObject response into a JSONArray called categoriesArr.
+                try {
+                    categoriesArr = response.toJSONArray(response.names());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Go through all the categories in response.
+                for (int i = 0; i < categoriesArr.length(); i++) {
+                    try {
+                        JSONObject curCategory = categoriesArr.getJSONObject(i);
+
+                        // First check if this material type is for upgrading characters (there were some weapon only materials when I looked through the response).
+                        if (curCategory.getString("weapons") != null) {
+                            JSONArray itemArr = curCategory.getJSONArray("items");
+                            // Each item has an attribute called "rarity", and I use that to sort them into the correct ArrayList.
+                            for (int j = 0; j < itemArr.length(); j++) {
+                                JSONObject curItem = itemArr.getJSONObject(j);
+                                // I reversed the order (rarity 3 = blue, but put into possibleIcons[0]) because
+                                // the for loop that puts the images into the ImageViews below,
+                                // starts with the highest rarity at the lowest index, mirroring allImgViewIcons[].
+                                switch (curItem.getInt("rarity")) {
+                                    case 5:
+                                        possibleIcons[0].add(curItem.getString("id"));
+                                        break;
+                                    case 4:
+                                        possibleIcons[1].add(curItem.getString("id"));
+                                        break;
+                                    case 3:
+                                        possibleIcons[2].add(curItem.getString("id"));
+                                        break;
+                                    case 2:
+                                        possibleIcons[3].add(curItem.getString("id"));
+                                        break;
+                                }
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        Log.i("API", "categoriesArr loop failed: " + e.toString());
+                    }
+                }
+//                txtStatic.setText(possibleIcons[0].toString() + "\n" + possibleIcons[1].toString() + "\n" + possibleIcons[2].toString() + "\n" + possibleIcons[3].toString());
+
+                // Skips imgViewGrey
+                for (int i = 0; i < (allImgViewIcons.length - 1); i++) {
+                    // Uses superclass' method to display the images.
+                    updateCounterIcon(allImgViewIcons[i], requestUrl0 + "/" + possibleIcons[i].get((int) (Math.random() * possibleIcons[i].size())));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "API request for image failed.", Toast.LENGTH_SHORT).show();
+                Log.i("API", "Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mJsonRequest);
     }
 }
